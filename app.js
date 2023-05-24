106f277abdf0e17837b06a8e1efc6cf6f75c74f4
ruchirtoshniwal <ruchirtoshniwal450@gmail.com>
2023-05-24 15:51:18 +0530

    Update issuer id and class id

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const bodyParser = require('body-parser');
 const { GoogleAuth } = require('google-auth-library'); 
 const jwt = require('jsonwebtoken');
 
+
 const serviceAccountFile = process.env.GOOGLE_APPLICATION_CREDENTIALS || './tribal-drake-387709-a3da52f2f148.json';
 const issuerId = process.env.WALLET_ISSUER_ID || '3388000000022224969';
 const classId = process.env.WALLET_CLASS_ID || '8af797d3-acb6-493a-aae5-cafb7226073f';
